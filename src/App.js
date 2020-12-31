7d800cd3091b13abd5b4b6fa6a3b18fc19f52fc6
gonta1026 <katano.original@gmail.com>
2021-01-01 08:57:14 +0900

    useState study

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
-function App() {
+const App = ({initName, initPrice}) => {
+  const [name, SetName] = useState(initName);
+  const [price, SetPrice] = useState(initPrice);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        現在の{name}は{price}です。
+      </div>
+    </>
+    
   );
+}
+
+App.defaultProps = {
+  initName: "",
+  initPrice: 1000
 }
 
 export default App;
