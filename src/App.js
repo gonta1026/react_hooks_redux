378b80dc42268ef04de466a892e2a4a09c8de06b
gonta1026 <katano.original@gmail.com>
2021-01-01 09:16:08 +0900

    implements fix

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
 import React, {useState} from 'react';
 
-const App = ({initName, initPrice}) => {
-  const [name, SetName] = useState(initName);
-  const [price, SetPrice] = useState(initPrice);
+const App = (props) => {
+  const [state, SetState] = useState(props);
+  console.log(state)
+  const { name, price } = state 
   return (
     <>
       <div>
         現在の{name}は{price}です。
       </div>
+      <button onClick={() => SetState({...state, price: price + 1})}> +</button>
+      <button onClick={() => SetState({...state, price: price - 1})}> -</button>
+      <button onClick={() => SetState(props)}>リセット</button>
+      <input type="text" value={name} onChange={(e) => SetState({...state, name: e.target.value})}/>
     </>
     
   );
 }
 
 App.defaultProps = {
-  initName: "",
-  initPrice: 1000
+  name: "",
+  price: 1000
 }
 
 export default App;
