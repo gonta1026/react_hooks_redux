c5384fc9a1ba81e806098d6d48c16948fe779bc2
gonta1026 <katano.original@gmail.com>
2021-01-01 09:32:21 +0900

    implements fix

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const App = (props) => {
   const [state, SetState] = useState(props);
   console.log(state)
   const { name, price } = state 
+
+  useEffect(() => {
+    console.log("Didmount!!")
+  }, [])
+
+  const periot = () => {
+    console.log("periot")
+    return "。"
+  }
   return (
     <>
       <div>
-        現在の{name}は{price}です。
+        現在の{name}は{price}です{periot()}
       </div>
       <button onClick={() => SetState({...state, price: price + 1})}> +</button>
       <button onClick={() => SetState({...state, price: price - 1})}> -</button>
